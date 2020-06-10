0415083bde7863198c80fdd07147746b45b9d73e
mklmng <mikelmonge@gmail.com>
2020-06-10 19:24:59 +0100

    Updates

---
+++ precache-manifest.03a5cd25935f2080bf95d3546221e9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6351ba0ec8bd3397e567c7980c6692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9128f5881cd2d6310723",
+    "url": "/static/css/main.2dfbb023.chunk.css"
+  },
+  {
+    "revision": "1211fdf7723eaa752e25",
+    "url": "/static/js/2.b80f39e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b80f39e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9128f5881cd2d6310723",
+    "url": "/static/js/main.1e305ba2.chunk.js"
+  },
+  {
+    "revision": "ac8cbf2589ca0b715fe2",
+    "url": "/static/js/runtime-main.9afe67e5.js"
+  }
+]);
