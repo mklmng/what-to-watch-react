28a8849bd8ed48618eec2315f731bfd1e7b419b4
mklmng <mikelmonge@gmail.com>
2020-06-10 19:55:22 +0100

    Updates

---
+++ precache-manifest.820fc34b242fa374f1325faf018d7ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082a81615e9b770479e7cbfe8edd79a2",
+    "url": "/what-to-watch-app/index.html"
+  },
+  {
+    "revision": "bffb63380f6ef5e7df11",
+    "url": "/what-to-watch-app/static/css/main.2dfbb023.chunk.css"
+  },
+  {
+    "revision": "1211fdf7723eaa752e25",
+    "url": "/what-to-watch-app/static/js/2.b80f39e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/what-to-watch-app/static/js/2.b80f39e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffb63380f6ef5e7df11",
+    "url": "/what-to-watch-app/static/js/main.96455c8a.chunk.js"
+  },
+  {
+    "revision": "ce5bd46f57b3d5b7f209",
+    "url": "/what-to-watch-app/static/js/runtime-main.42e1fffe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03a5cd25935f2080bf95d3546221e9ed.js"
+  "/what-to-watch-app/precache-manifest.820fc34b242fa374f1325faf018d7ae2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/what-to-watch-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
