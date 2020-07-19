8d2e89066d6f8964b3247c39d4787f02cc0dafac
mklmng <mikelmonge@gmail.com>
2020-07-19 12:49:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -335,7 +335,6 @@ class App extends Component {
     return (
       <div id="full-wrapper">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" type="text/css" />
 
         {overlay &&  <FilmOverlay handleToggleOverlay={this.handleToggleOverlay} trailer={trailer} /> }
 
