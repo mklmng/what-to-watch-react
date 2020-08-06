c7e7e766f8fe081910c25b9c8388775f4ee59678
mklmng <mikelmonge@gmail.com>
2020-08-06 14:46:01 +0100

    Moved Boostrap CSS to index file.

--- src/App.js
+++ src/App.js
@@ -559,10 +559,7 @@ class App extends Component {
 
     return (
       <div id="full-wrapper">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
-
         {overlay &&  <FilmOverlay handleToggleOverlay={this.handleToggleOverlay} trailer={trailer} /> }
-
         <div className="container">
           <div className="row below1">
             <Header />
