5aa5037e4651fee42b726b7e8333389b55c7cec0
mklmng <mikelmonge@gmail.com>
2021-02-12 22:57:39 +0100

    Updates

---
+++ precache-manifest.1ff8bc0395fcf8f511323124da66501e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4976b4ad72f8a02bec5044495d9e5dc5",
+    "url": "/what-to-watch-react/index.html"
+  },
+  {
+    "revision": "10201d08708439076fb6",
+    "url": "/what-to-watch-react/static/css/main.d2b0f168.chunk.css"
+  },
+  {
+    "revision": "483cba9823b9d97cbfdc",
+    "url": "/what-to-watch-react/static/js/2.6689520a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/what-to-watch-react/static/js/2.6689520a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10201d08708439076fb6",
+    "url": "/what-to-watch-react/static/js/main.ca25d3f7.chunk.js"
+  },
+  {
+    "revision": "007146a48bfb7a3c4711",
+    "url": "/what-to-watch-react/static/js/runtime-main.317e5aa5.js"
+  },
+  {
+    "revision": "82481d53eb354b043b86b87d0e1585f8",
+    "url": "/what-to-watch-react/static/media/magnifying-glass-icon.82481d53.svg"
+  },
+  {
+    "revision": "6d099ba0d0a5ddeebee231b16081a776",
+    "url": "/what-to-watch-react/static/media/watched-icon.6d099ba0.svg"
+  }
+]);
